9980d8fd9c5c8cd51dc7a899c53fe1507b24c178
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-05-09 22:18:56 +0300

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
-
+// the user is redirected to the login page if a user is not logged in 
 function Home({ isLoggedIn }) {
   if (!isLoggedIn) return <Redirect to="/login" />;
-
+// if not it its default to home page 
   return (
     <div>
       <h1>Home!</h1>
